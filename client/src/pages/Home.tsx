51531395931f74626e317959a441b967151de252
Kevin Cheddar <kevincheddar@gmail.com>
2024-11-24 20:22:56 -0500

    Added basic home styling / branding

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,20 +1,43 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import WorshipSyncImage from "../assets/WorshipSyncImage.png";
+import Button from "../components/Button/Button";
+import { ReactNode } from "react";
+
+const LinkButton = ({ to, children }: { to: string; children: ReactNode }) => {
+  return (
+    <Button
+      padding="p-0"
+      variant={to === "/controller" ? "primary" : "tertiary"}
+    >
+      <Link className="h-full w-full px-4 py-2" to={to}>
+        {children}
+      </Link>
+    </Button>
+  );
+};
 
 const Welcome = () => {
-    return (
-        <main>
-            <h1>Welcome to Portable Media</h1>
-            <p>This software is in beta and works best with chromium browsers like Edge and Chrome</p>
-            <section className="flex flex-col">
-              <Link to="/controller">Controller</Link>
-              <Link to="/monitor">Monitor</Link>
-              <Link to="/presentation">Presentation</Link>
-              <Link to="/stream">Stream</Link>
-            </section>
+  return (
+    <main className="bg-slate-700 h-screen w-screen text-white">
+      <img
+        src={WorshipSyncImage}
+        alt="WorshipSyncImage"
+        className="max-w-[75%] mx-auto"
+        width={400}
+        height={367}
+      />
+      <p className="text-lg text-center w-full px-4">
+        This software is in beta and works best with chromium based browsers
+        like Edge and Chrome
+      </p>
+      <section className="flex flex-col text-2xl mt-8 gap-4 bg-slate-800 w-full items-center p-8">
+        <LinkButton to="/controller">Controller</LinkButton>
+        <LinkButton to="/monitor">Monitor</LinkButton>
+        <LinkButton to="/presentation">Presentation</LinkButton>
+        <LinkButton to="/stream">Stream</LinkButton>
+      </section>
+    </main>
+  );
+};
 
-        </main>
-
-    )
-}
-
-export default Welcome
+export default Welcome;
