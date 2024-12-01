df92ab7eac6fc24180d35d23cf57b64e7a7d8626
Kevin Cheddar <kevincheddar@gmail.com>
2024-12-01 18:38:58 -0500

    Add instructions for projector/monitor

--- client/src/containers/PresentationPage.tsx
+++ client/src/containers/PresentationPage.tsx
@@ -4,45 +4,51 @@ import Button from "../components/Button/Button";
 import { Presentation as PresentationType } from "../types";
 
 type PresentationProps = {
-  displayInfo: PresentationType
-  prevDisplayInfo: PresentationType
-}
+  displayInfo: PresentationType;
+  prevDisplayInfo: PresentationType;
+};
 
-const Presentation = ({ displayInfo, prevDisplayInfo } : PresentationProps) => {
+const Presentation = ({ displayInfo, prevDisplayInfo }: PresentationProps) => {
   const [isFullscreen, setIsFullscreen] = useState(false);
 
   const handleClick = () => {
     document.body.requestFullscreen();
-  }
+  };
 
   useEffect(() => {
     const onFullScreenChange = () => {
       setIsFullscreen(document.fullscreenElement !== null);
-    }
+    };
 
-    document.addEventListener('fullscreenchange', onFullScreenChange);
+    document.addEventListener("fullscreenchange", onFullScreenChange);
     return () => {
-      document.removeEventListener('fullscreenchange', onFullScreenChange);
-    }
-  }, [])
+      document.removeEventListener("fullscreenchange", onFullScreenChange);
+    };
+  }, []);
 
-  return (
-    isFullscreen ? (
-      <DisplayWindow 
-        boxes={displayInfo.slide?.boxes || []} 
-        prevBoxes={prevDisplayInfo.slide?.boxes || []}
-        displayType={displayInfo.displayType}
-        shouldAnimate
-        width={100}
-      />
-    ) : (
-      <div className="h-screen w-screen flex items-center justify-center bg-black">
-        <Button variant="cta" onClick={handleClick} className="text-7xl">
-          Click to go Fullscreen
-        </Button>
-      </div>
-    )
-  )
-}
+  return isFullscreen ? (
+    <DisplayWindow
+      boxes={displayInfo.slide?.boxes || []}
+      prevBoxes={prevDisplayInfo.slide?.boxes || []}
+      displayType={displayInfo.displayType}
+      shouldAnimate
+      width={100}
+    />
+  ) : (
+    <div className="h-screen w-screen flex flex-col items-center justify-center bg-black gap-10">
+      <p className="text-5xl p-6 bg-gray-700 text-yellow-300 text-center">
+        Drag this window to the intended display
+      </p>
+      <Button
+        variant="cta"
+        onClick={handleClick}
+        className="text-7xl"
+        padding="px-6 py-2"
+      >
+        Click to go Fullscreen
+      </Button>
+    </div>
+  );
+};
 
 export default Presentation;
