03e77e4dfa5c3180cd17553e506a543e32a4b08f
Kevin Cheddar <kevincheddar@gmail.com>
2025-07-27 23:39:03 -0400

    fix: Improved UI responsiveness and readability

---
+++ client/src/components/Button/ButtonGroupItem.tsx
@@ -0,0 +1,24 @@
+import React, { forwardRef } from "react";
+import Button, { ButtonProps } from "./Button";
+import cn from "classnames";
+
+export type ButtonGroupItemProps = ButtonProps & {
+  isActive?: boolean;
+};
+
+const ButtonGroupItem = forwardRef<HTMLButtonElement, ButtonGroupItemProps>(
+  ({ className, isActive, ...props }, ref) => {
+    return (
+      <Button
+        ref={ref}
+        className={cn("button-group-item", className)}
+        variant={isActive ? "secondary" : "tertiary"}
+        {...props}
+      />
+    );
+  }
+);
+
+ButtonGroupItem.displayName = "ButtonGroupItem";
+
+export default ButtonGroupItem;
