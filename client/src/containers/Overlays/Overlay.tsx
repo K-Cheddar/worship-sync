92ee772fc29ff85641b5e0e9acddb17f6bdc4a5d
Kevin Cheddar <kevincheddar@gmail.com>
2026-01-06 19:02:56 -0500

    feat: Add full app zoom

--- client/src/containers/Overlays/Overlay.tsx
+++ client/src/containers/Overlays/Overlay.tsx
@@ -16,6 +16,7 @@ import { CSS } from "@dnd-kit/utilities";
 import { useSortable } from "@dnd-kit/sortable";
 import { useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
+import { cn } from "../../utils/cnHelper";
 
 type OverlayProps = {
   overlay: OverlayInfo;
@@ -157,6 +158,8 @@ const Overlay = ({
     }, 500);
   };
 
+  const overlayBaseClass = "flex items-center h-full text-wrap text-center";
+
   return (
     <li
       className={`flex items-center rounded-lg w-full overflow-clip leading-3 ${
@@ -182,17 +185,17 @@ const Overlay = ({
         {overlay.type === "participant" && (
           <>
             {overlay.name && (
-              <span className="text-base flex items-center h-full">
+              <span className={cn(overlayBaseClass, "text-base")}>
                 {overlay.name}
               </span>
             )}
             {overlay.title && (
-              <span className="flex text-sm items-center h-full italic">
+              <span className={cn(overlayBaseClass, "text-sm italic")}>
                 {overlay.title}
               </span>
             )}
             {overlay.event && (
-              <span className="flex text-sm items-center h-full">
+              <span className={cn(overlayBaseClass, "text-sm")}>
                 {overlay.event}
               </span>
             )}
@@ -201,12 +204,12 @@ const Overlay = ({
         {overlay.type === "stick-to-bottom" && (
           <>
             {overlay.heading && (
-              <span className="flex text-sm items-center h-full">
+              <span className={cn(overlayBaseClass, "text-sm")}>
                 {overlay.heading}
               </span>
             )}
             {overlay.subHeading && (
-              <span className="flex text-sm items-center h-full">
+              <span className={cn(overlayBaseClass, "text-sm")}>
                 {overlay.subHeading}
               </span>
             )}
@@ -216,7 +219,7 @@ const Overlay = ({
         {overlay.type === "qr-code" && (
           <>
             {overlay.description && (
-              <span className="flex text-sm items-center h-full">
+              <span className={cn(overlayBaseClass, "text-sm")}>
                 {overlay.description.split("\n")[0]}
               </span>
             )}
@@ -226,7 +229,7 @@ const Overlay = ({
         {overlay.type === "image" && (
           <>
             {overlay.name && (
-              <span className="flex text-sm items-center h-full">
+              <span className={cn(overlayBaseClass, "text-sm")}>
                 {overlay.name}
               </span>
             )}
@@ -234,7 +237,7 @@ const Overlay = ({
         )}
 
         {!hasData && (
-          <span className="text-sm leading-7">
+          <span className={cn(overlayBaseClass, "text-sm")}>
             Click to add overlay details
           </span>
         )}
